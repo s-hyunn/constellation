850e722c05ff79a4e3ad73eb2855d0022cbeeba7
Yamtan1 <gndndk12305@naver.com>
2025-06-23 15:38:36 +0900

    12313

--- src/main/java/com/example/constellation/controller/ConstellationController.java
+++ src/main/java/com/example/constellation/controller/ConstellationController.java
@@ -1,12 +1,20 @@
 package com.example.constellation.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.example.constellation.dao.IConstellationDAO;
+import com.example.constellation.dto.ConstellationDto;
+
 @Controller
 public class ConstellationController {
+	
+	@Autowired
+	IConstellationDAO condao;
 	
 	@GetMapping("/")
 	public String root() {
@@ -14,16 +22,18 @@ public class ConstellationController {
 	}
 	
 	@GetMapping("/list")
-	public String list(@RequestParam("id") int id) {
-		
+	public String list(Model model) {
+		model.addAttribute("stars", condao.selectAll());
 		return "list";
 	}
-	@GetMapping("/form")
+	@GetMapping("/edit/{id}\")
 	public String form() {
 		return "form";
 	}
 	@PostMapping("/result")
-	public String result() {
+	public String result(@RequestParam("id") int id, Model model) {
+		ConstellationDto cd = condao.selectById(id);
+		model.addAttribute("star", cd);
 		return "result";
 	}
 	
--- src/main/java/com/example/constellation/dao/IConstellationDAO.java
+++ src/main/java/com/example/constellation/dao/IConstellationDAO.java
@@ -3,6 +3,7 @@ package com.example.constellation.dao;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import com.example.constellation.dto.ConstellationDto;
 
@@ -10,4 +11,5 @@ import com.example.constellation.dto.ConstellationDto;
 public interface IConstellationDAO {
 	
 	List<ConstellationDto> selectAll();
+	ConstellationDto selectById(@Param("id")int id);
 }
