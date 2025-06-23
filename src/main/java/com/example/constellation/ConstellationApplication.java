3366754ef9332e9ad958bb1de73f1dae6988edab
s-hyunn <cktngus4@naver.com>
2025-06-23 14:38:03 +0900

    🚀 프로젝트 초기 커밋

---
+++ src/main/java/com/example/constellation/ConstellationApplication.java
@@ -0,0 +1,13 @@
+package com.example.constellation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConstellationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConstellationApplication.class, args);
+	}
+
+}
